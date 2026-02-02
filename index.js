def0b23659efc69a291f0439e8b9aee49916d3be
dcardoso41-svg <dcardoso41@gmail.com>
2026-02-02 15:00:44 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+{
+  "name": "kick-chat-bridge",
+  "version": "1.0.0",
+  "type": "module",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "@retconned/kick-js": "^1.0.0",
+    "dotenv": "^16.4.5"
+  },
+  "engines": {
+    "node": ">=18.0.0"
+  }
+}
